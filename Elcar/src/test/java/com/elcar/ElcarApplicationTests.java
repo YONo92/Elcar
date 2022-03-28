ceac7549582bac88f5141e469c279f51336b1044
YONo92 <89058176+YONo92@users.noreply.github.com>
2022-03-28 10:35:34 +0900

    first commmit

---
+++ Elcar/src/test/java/com/elcar/ElcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
