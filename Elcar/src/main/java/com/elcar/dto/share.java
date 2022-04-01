37d6b90124ff1724ab5379efa6b1a447dccf1c67
Cha Hyun <ckgus265@gmail.com>
2022-04-01 14:38:48 +0900

    share DTO 컨트롤러 생성/ commit

--- Elcar/src/main/java/com/elcar/dto/share.java
+++
@@ -1,6 +0,0 @@
-package com.elcar.dto;
-
-public class share {
-
-	
-}
--- Elcar/src/main/java/com/elcar/share/shareController.java
+++ Elcar/src/main/java/com/elcar/share/shareController.java
@@ -1,13 +1,19 @@
 package com.elcar.share;
 
-import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.elcar.dto.Share;
 
 @Controller
 public class shareController {
 
 	@GetMapping("sharelist")
-	public ModelAndView shareList(@ManagedAttribute )
+	public ModelAndView shareList(@ModelAttribute Share share) {
+		ModelAndView mav = new ModelAndView();
+		
+		return mav;
+	}
 }
