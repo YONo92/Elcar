aaba1256ecdc009f1e66d77629c52765143482d7
Cha Hyun <ckgus265@gmail.com>
2022-04-01 14:22:55 +0900

    test commit

---
+++ Elcar/src/main/java/com/elcar/dto/share.java
@@ -0,0 +1,6 @@
+package com.elcar.dto;
+
+public class share {
+
+	
+}
--- Elcar/src/main/java/com/elcar/share/shareController.java
+++ Elcar/src/main/java/com/elcar/share/shareController.java
@@ -1,5 +1,13 @@
 package com.elcar.share;
 
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class shareController {
 
+	@GetMapping("sharelist")
+	public ModelAndView shareList(@ManagedAttribute )
 }
